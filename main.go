43fdf6b0733eb074ffb74ddfc1ea54647a54395b
Marko Mikulicic <mmikulicic@gmail.com>
2022-07-01 02:19:01 +0200

    More realistic load

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/jamiealquiza/tachymeter"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/binarylog/grpc_binarylog_v1"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/protobuf/types/known/timestamppb"
 	v1 "mkm.pub/binlog/proto"
@@ -28,7 +29,7 @@ func mainE() error {
 	errCh := make(chan error)
 	prefix := rand.Intn(1024 * 1024 * 1024)
 
-	for i := 0; i < 200; i++ {
+	for i := 0; i < 50; i++ {
 		go func() {
 			errCh <- worker(ctx, prefix, sink)
 		}()
@@ -45,26 +46,60 @@ func worker(ctx context.Context, prefix int, sink v1.LogSinkServiceClient) error
 		if i%100 == 0 {
 			log.Printf("%d: tick %d, p90: %v", prefix, i, c.Calc().Time.P99)
 		}
+		traceID := fmt.Sprintf("%d-%d-%d", prefix, prefix2, i)
 
-		req := &v1.WriteRequest{
-			Origin: "tsm",
-			CallId: fmt.Sprintf("%d-%d-%d", prefix, prefix2, i),
-			Entry: &grpc_binarylog_v1.GrpcLogEntry{
-				Timestamp: timestamppb.Now(),
-				CallId:    uint64(i),
-				Type:      grpc_binarylog_v1.GrpcLogEntry_EVENT_TYPE_CLIENT_HEADER,
+		entries := []grpc_binarylog_v1.GrpcLogEntry{
+			{
+				Type: grpc_binarylog_v1.GrpcLogEntry_EVENT_TYPE_CLIENT_HEADER,
+				Payload: &grpc_binarylog_v1.GrpcLogEntry_ClientHeader{
+					ClientHeader: &grpc_binarylog_v1.ClientHeader{
+						MethodName: "dummy",
+						Metadata: &grpc_binarylog_v1.Metadata{
+							Entry: []*grpc_binarylog_v1.MetadataEntry{
+								{Key: "influx-trace-id", Value: []byte(traceID)},
+							},
+						},
+					},
+				},
+			},
+			{
+				Type: grpc_binarylog_v1.GrpcLogEntry_EVENT_TYPE_SERVER_TRAILER,
+				Payload: &grpc_binarylog_v1.GrpcLogEntry_Trailer{
+					Trailer: &grpc_binarylog_v1.Trailer{StatusCode: uint32(codes.Unknown)},
+				},
 			},
 		}
-		start := time.Now()
-		_, err := sink.Write(ctx, req)
-		c.AddTime(time.Since(start))
 
-		if err != nil {
-			return err
+		origins := []string{"tsm", "iox"}
+		for s, entry := range entries {
+			for o := 0; o < len(origins); o++ {
+				if err := write(ctx, sink, origins[o], c, traceID, i, s, &entry); err != nil {
+					return err
+				}
+			}
 		}
-
 	}
+}
 
+func write(ctx context.Context, sink v1.LogSinkServiceClient, origin string, c *tachymeter.Tachymeter, traceID string, i int, s int, entry *grpc_binarylog_v1.GrpcLogEntry) error {
+	entry.Timestamp = timestamppb.Now()
+	entry.CallId = uint64(i)
+	entry.SequenceIdWithinCall = uint64(s)
+
+	uniqueCallID := fmt.Sprintf("%s-%s", origin, traceID)
+
+	req := &v1.WriteRequest{
+		Origin: origin,
+		CallId: uniqueCallID,
+		Entry:  entry,
+	}
+	start := time.Now()
+	_, err := sink.Write(ctx, req)
+	c.AddTime(time.Since(start))
+
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
